6721a9cda5d7afd1cff908ee48ed27796d4813d8
hideki watabe <hideki.w.1971@gmail.com>
2022-09-24 21:32:53 +0900

    2022/9/24 swiper(with thumbs)作成中。まだ連動しない。

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
 jQuery(function ($) {
   // この中であればWordpressでも「$」が使用可能になる
-
   // swiper
   var swiper = new Swiper(".mySwiper", {
     loop: true,
     spaceBetween: 10,
-    slidesPerView: 5,
+    slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
   });
   var swiper2 = new Swiper(".mySwiper2", {
     loop: true,
